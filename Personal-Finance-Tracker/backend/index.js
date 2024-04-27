7963c99300bc57bd8fd6a877187a4a98793f4adc
Keido0208 <50084889+Keido0208@users.noreply.github.com>
2024-04-27 18:15:54 -0500

    Create connection between server and client side

--- Personal-Finance-Tracker/backend/index.js
+++ Personal-Finance-Tracker/backend/index.js
@@ -1,14 +1,21 @@
 // server.js
 const express = require("express");
-const app = express();
-const port = 3000;
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-app.use(express.json());
+const app = express();
+const port = 5000;
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
+app.post("/data", (req, res) => {
+  console.log(req.body.budgets);
+  res.send({ status: "Data received!", yourInput: req.body.budgets });
+});
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
